146f0942b4d69f455c3a0f68c8f5c55cda0ef64b
JGenBCC <jgenbcc@gmail.com>
2025-02-02 20:52:24 +0530

    user-info screen 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+//export default nextConfig;
+
+export default {
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "Cross-Origin-Opener-Policy",
+              value: "same-origin-allow-popups",
+            },
+            {
+              key: "Cross-Origin-Embedder-Policy",
+              value: "require-corp",
+            },
+          ],
+        },
+      ];
+    },
+};
